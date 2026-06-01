a5253ea9326593bd230187282cff5b4ab6962e27
Randy <randymotaj@gmail.com>
2026-06-01 19:03:48 -0400

    Finishing the text part of the hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from "./components/Header.tsx";
+import Hero from "./components/Hero.tsx";
 
 function App() {
 
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,32 @@
+import "../styles/Hero.css";
+import { TbArrowBadgeRightFilled } from "react-icons/tb";
+
+function Hero () {
+  return (
+    <div className="hero-container">
+
+      <div className="hero-text-container">
+        <div className="state">
+          <span className="dot"></span>
+          <span>AVAILABLE FOR PROJECTS</span>
+        </div>
+        <h1>ENGINERING <span className="green">THE DIGITAL</span> FUTURE</h1>
+        <p>Specializing in <span className="highlight">high-performance distributed systems</span> and avant-garde UI architectures. I build tools that redefine efficiency — from cloud orchestration to real-time analytics interfaces.</p>
+
+        <div className="buttons-container">
+          <button className="explore">EXPLORE WORK
+            <TbArrowBadgeRightFilled className="arrow"/></button>
+          <button className="stack">VIEW THE STACK</button>
+        </div>
+      </div>
+
+      <div className="hero-image">
+        
+      </div>
+
+    </div>
+  );
+}
+
+export default Hero;
+
